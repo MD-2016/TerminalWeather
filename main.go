bd484341f5607cccb212e2952aa704ff61302c5f
MD-2016 <25043596+MD-2016@users.noreply.github.com>
2023-07-15 14:07:29 -0400

    Successfully pulled weather data for india. Need to add F or C options and handle countries with states in url input

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"regexp"
 	"strings"
 
 	"github.com/gocolly/colly"
@@ -12,7 +13,6 @@ import (
 type WeatherReport struct {
 	Location  string
 	Temp      string
-	Scale     string
 	Condition string
 }
 
@@ -29,10 +29,12 @@ func main() {
 	}
 
 	inputSlice := strings.Split(input, ",")
-	fmt.Println(inputSlice)
+	//fmt.Println(inputSlice)
 
-	weather := GetReport(input)
-	fmt.Printf("The temp in %s is %s %s and %s.", weather.Location, weather.Temp, weather.Scale, weather.Condition)
+	//testStrings()
+
+	weather := GetReport(inputSlice)
+	fmt.Printf("The temp in %s is %s and %s.", weather.Location, weather.Temp, weather.Condition)
 }
 
 func PrintHead() {
@@ -40,8 +42,24 @@ func PrintHead() {
 
 }
 
-func GetReport(zipcode string) WeatherReport {
-	//url := fmt.Sprintf("https://www.wunderground.com/weather/%s/%s")
+func GetReport(input []string) WeatherReport {
+	countryAbbrev := input[0]
+	fmt.Println(countryAbbrev)
+	city := input[1]
+	city = strings.TrimSpace(city)
+	cityByte := []byte(city)
+	//fmt.Println(city)
+	matched, err := regexp.Match("[^A-Za-z]", cityByte)
+	formattedCityString := ""
+	var citySlice []string
+	if err != nil {
+		fmt.Println("The string doesn't match")
+	}
+	if matched {
+		citySlice = strings.Split(city, " ")
+		formattedCityString = strings.Join(citySlice, "-")
+	}
+	url := fmt.Sprintf("https://www.wunderground.com/weather/%s/%s", countryAbbrev, formattedCityString)
 	rep := WeatherReport{}
 	c := colly.NewCollector()
 
@@ -50,21 +68,35 @@ func GetReport(zipcode string) WeatherReport {
 	})
 
 	c.OnHTML(".city-header > h1:nth-child(2) > span:nth-child(1)", func(e *colly.HTMLElement) {
-		rep.Location = e.Text
+
+		res := strings.Split(e.Text, " Weather Conditions")
+		//fmt.Println(res)
+		rep.Location = strings.Join(res, " ")
 	})
 
 	c.OnHTML(".condition-icon > p:nth-child(2)", func(e *colly.HTMLElement) {
 		rep.Condition = e.Text
 	})
 
-	c.OnHTML(".current=temp > lib-display-unit:nth-child(1) > span:nth-child(1) > span:nth-child(1)", func(e *colly.HTMLElement) {
+	c.OnHTML(".current-temp > lib-display-unit:nth-child(1) > span:nth-child(1) > span:nth-child(1)", func(e *colly.HTMLElement) {
 		rep.Temp = e.Text
 	})
 
-	c.OnHTML(".current-temp > lib-display-unit:nth-child(1) > span:nth-child(1) > span:nth-child(2) > span:nth-child(1)", func(e *colly.HTMLElement) {
-		rep.Scale = e.Text
-	})
-
-	//c.Visit(url)
+	c.Visit(url)
 	return rep
 }
+
+/*
+func testStrings() {
+	test := "New Delhi, Delhi, India Weather Conditions"
+	fmt.Println(test)
+	split := strings.Split(test, " Weather Conditions")
+	fmt.Println(split)
+	res := strings.Join(split, " ")
+	fmt.Println(res)
+
+}
+*/
+
+
+func 
